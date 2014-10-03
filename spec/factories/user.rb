fc1c60d9d7eadb691c7e06d46ceb64d87997e492
David Askew <davidaskew@gmail.com>
2014-10-03 12:13:20 -0400

    Rspec Setup

---
+++ spec/factories/user.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password "awfulPass"
+    password_confirmation "awfulPass"
+  end
+
+  #trait :seller do
+  #  role 'seller'
+  #end
+
+end
